6bf236ae358938b1528f11d3803fef2af73ba422
Gustavo Evangelista <gustavo.eng.henrique@gmail.com>
2025-05-18 18:33:12 +0100

    first commit

---
+++ fibonacci-sequence.js
@@ -0,0 +1,7 @@
+//give a number "n", find the first 'n' elements of the fibonacci sequence
+// The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones, usually starting with 0 and 1. The sequence commonly starts 0, 1, 1, 2, 3, 5, 8, 13, 21, and so on.
+
+function fibonacci(n) {
+    
+    
+}
